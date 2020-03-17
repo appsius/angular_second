4c7f469f85e6dc9e78f4ed6ab4a0e69307d712b1
appsius <ilyas.ustunkaya234434@gmail.com>
2020-03-17 10:14:09 +0300

    Showing or Hiding the Elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
-import { AuthService } from './../services/auth.service';
-import { Component } from '@angular/core';
+import { AuthService } from "./../services/auth.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent {
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 }
