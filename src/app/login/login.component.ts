3f764617fdcc144b7dd7b5e5ec9ad3a2364118f8
appsius <ilyas.ustunkaya234434@gmail.com>
2020-03-17 12:36:56 +0300

    Redirecting Users After Logged In

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { AuthService } from "./../services/auth.service";
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "login",
@@ -10,12 +10,20 @@ import { Router } from "@angular/router";
 export class LoginComponent {
   invalidLogin: boolean;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   signIn(credentials) {
     this.authService.login(credentials).subscribe(result => {
-      if (result) this.router.navigate(["/"]);
-      else this.invalidLogin = true;
+      if (result) {
+        let returnUrl = this.route.snapshot.queryParamMap.get("returnUrl");
+        this.router.navigate([returnUrl || "/"]);
+      } else {
+        this.invalidLogin = true;
+      }
     });
   }
 }
--- src/app/src/app/services/auth-guard.service.ts
+++ src/app/src/app/services/auth-guard.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "app/services/auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {}
 
-  canActivate() {
+  canActivate(router, state: RouterStateSnapshot) {
     if (this.authService.isLoggedIn()) return true;
 
-    this.router.navigate(["/login"]);
+    this.router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
     return false;
   }
 }
